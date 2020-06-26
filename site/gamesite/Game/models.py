c7506091d0a87b845aaa3e9396f049a9d5e04177
1styrGroupB <1styrGroupB@pn.local>
2020-06-26 21:40:46 +0800

    second commit

--- site/gamesite/Game/models.py
+++ site/gamesite/Game/models.py
@@ -5,23 +5,11 @@ from django.db import models
 class Game(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # category = models.
-    # game categories
-    # Action
-    # games
-    # Action - adventure
-    # games
-    # Adventure
-    # games
-    # Role - playing
-    # games
-    # Simulation
-    # games
-    # Strategy
-    # games
-    # Sports
-    # games
-    # Puzzle
-    # games
-    # Idle
-    # games
+    date_published = models.DateField(auto_now_add=True)
+    game_images = models.ImageField(default='default.png',upload_to='media')
+    slug = models.SlugField(max_length=200)
+    # game file
+    # author = 
+    # category = models.CharField(choices=)
+    def __str__ (self):
+        return self.name
--- site/gamesite/Game/views.py
+++ site/gamesite/Game/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import Game
 # Create your views here.
 
 
 def home(request):
-    return render(request,'Game/home.html')
+    games = Game.objects.all()
+    return render(request,'Game/home.html',{'games': games})
+    
