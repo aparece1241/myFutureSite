2d6465cb9bae89beddf2d7e5a44efc44af061451
aparece1241 <apareceh7@gmail.com>
2020-06-26 18:34:58 +0800

    adds some changes

--- site/gamesite/Game/models.py
+++ site/gamesite/Game/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # category = models.
+    # game categories
+    # Action
+    # games
+    # Action - adventure
+    # games
+    # Adventure
+    # games
+    # Role - playing
+    # games
+    # Simulation
+    # games
+    # Strategy
+    # games
+    # Sports
+    # games
+    # Puzzle
+    # games
+    # Idle
+    # games
